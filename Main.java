d1d0addc81417e899718e5740ba469b3654e3f88
tharunvulava <90199079+tharunvulava@users.noreply.github.com>
2022-08-15 19:06:11 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    ArrayList <Integer> al=new ArrayList();
+        al.add(30);                 
+        al.add(9);
+        al.add(2000);
+        al.add(8);
+        al.add(10);
+        al.add(2006);
+		for(Integer i:al)
+		{
+		    System.out.println(i);
+		}
+	}
+}
